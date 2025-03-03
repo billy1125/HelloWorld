25dda225f1a0e9478bb3ede900cd38ce8d0b76c8
Cho-Hsun Lu <billy1125@gmail.com>
2025-03-03 15:50:50 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGO = new System.Windows.Forms.Button();
+            this.txtShow = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnGO
+            // 
+            this.btnGO.Font = new System.Drawing.Font("微軟正黑體", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnGO.Location = new System.Drawing.Point(326, 75);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(168, 78);
+            this.btnGO.TabIndex = 0;
+            this.btnGO.Text = "開始";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // txtShow
+            // 
+            this.txtShow.Location = new System.Drawing.Point(23, 56);
+            this.txtShow.Name = "txtShow";
+            this.txtShow.Size = new System.Drawing.Size(249, 29);
+            this.txtShow.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 153);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(249, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 506);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtShow);
+            this.Controls.Add(this.btnGO);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.TextBox txtShow;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "佛光大學"; //你可以直接複製貼過去
+            txtShow.Text = "川普很帥";
+        }
+    }
+}
+
